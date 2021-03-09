8856f8610bf248292909c78deaa656cabad90e02
dylan <dylanhogg@gmail.com>
2021-03-09 22:33:01 +1100

    Basic scoring and box display.

--- src/clients/javascript/app.js
+++ src/clients/javascript/app.js
@@ -2,6 +2,21 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+function handleAnswer(term, answer, correct_answer) {
+    if (correct_answer === null) {
+        term.echo('Game has not started.');
+    } else if (answer === correct_answer) {
+        state_correct++;
+        term.echo('Correct!');
+        term.echo('Score: ' + state_correct + "/" + state_total);
+        // TODO: add correct streak
+    } else {
+        state_incorrect++;
+        term.echo('Wrong :(');
+        term.echo('Score: ' + state_correct + "/" + state_total);
+    }
+}
+
 var qna_data = null;
 var qna_data_count = 0;
 var data_ready = false;
@@ -9,6 +24,9 @@ var product_name = null;
 var config_spinner_name = 'dots';
 var correct_answer = null;
 
+var state_correct = 0;
+var state_incorrect = 0;
+var state_total = 0;
 
 $(function($, undefined) {
     var spinners_url = 'spinners.json';
@@ -43,30 +61,27 @@ $(function($, undefined) {
 
         $('body').terminal({
             help: function() {
-                this.echo('Try typing "qn ec2", or "qn ec" and then hitting tab twice to autocomplete.');
+                this.echo('Try typing "play ec2", or "play ec" and then hitting tab twice to autocomplete.');
             },
             a: function() {
-                if (correct_answer == 1) {
-                    this.echo('Correct!!');
-                } else {
-                    this.echo('Wrong :(');
-                }
+                handleAnswer(this, 1, correct_answer)
+            },
+            A: function() {
+                handleAnswer(this, 1, correct_answer)
             },
             b: function() {
-                if (correct_answer == 2) {
-                    this.echo('Correct!!');
-                } else {
-                    this.echo('Wrong :(');
-                }
+                handleAnswer(this, 2, correct_answer)
+            },
+            B: function() {
+                handleAnswer(this, 2, correct_answer)
             },
             c: function() {
-                if (correct_answer == 3) {
-                    this.echo('Correct!!');
-                } else {
-                    this.echo('Wrong :(');
-                }
+                handleAnswer(this, 3, correct_answer)
             },
-            qn: function(product) {
+            C: function() {
+                handleAnswer(this, 3, correct_answer)
+            },
+            play: function(product) {
                 spinner = spinners[config_spinner_name];
                 start(this, spinner);
 
@@ -90,24 +105,24 @@ $(function($, undefined) {
                     var idx2 = randomNumber(0, qna_data_count);
                     var idx3 = randomNumber(0, qna_data_count);
 
-                    term.echo("\nAnswer:");
-                    term.echo(data[idx1]["answer"]);
-                    term.echo("\nQuestion A:");
-                    term.echo(data[idx1]["question"]);
-                    term.echo("\nQuestion B:");
-                    term.echo(data[idx2]["question"]);
-                    term.echo("\nQuestion C:");
-                    term.echo(data[idx3]["question"]);
-                    term.echo("\n");
+                    term.echo("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n┃ Answer                         ┃\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"
+                            + data[idx1]["answer"]);
+                    term.echo("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n┃ Question A                     ┃\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"
+                            + data[idx1]["question"]);
+                    term.echo("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n┃ Question B                     ┃\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"
+                            + data[idx2]["question"]);
+                    term.echo("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n┃ Question C                     ┃\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n"
+                            + data[idx3]["question"]);
+                    term.echo("");
 
                     correct_answer = 1;
+                    state_total++;
                   },
                   error: function(data) {
                     console.log('ajax error');
                     console.log(data);
                     var term = $.terminal.active()
                     stop(term, spinner);
-
                     term.echo("ajax error: " + data.status + " " + data.statusText + " for url: " + data_url);
                   },
                 });
@@ -115,10 +130,11 @@ $(function($, undefined) {
         }, {
             name: 'cloud_jeopardy',
             prompt: 'cloud-jeopardy> ',
-            greetings: 'Welcome to Cloud Jeopardy!\n\nAn AWS Certification study tool - select the correct question for the given AWS FAQ answer, Jeopardy style.\n',
+            greetings: 'Welcome to Cloud Jeopardy!\n\n' +
+                'An AWS Certification study tool - select the correct question for the given AWS FAQ answer, Jeopardy style.\n',
             scrollOnEcho: true,
             completion: function(command, callback) {
-                var utils = ['help', 'status', 'qn'];
+                var utils = ['help', 'status', 'play'];
                 var products = ['amazon-mq','amplify','api-gateway','app-mesh','app2container','appflow','application-discovery','appstream2','appsync','athena','audit-manager','augmented-ai','autoscaling','aws-transfer-family','backup','batch','braket','cdk','certificate-manager','chatbot','chime','cloud9','cloudformation','cloudfront','cloudhsm','cloudsearch','cloudshell','cloudtrail','cloudwatch','codebuild','codecommit','codedeploy','codeguru','codepipeline','codestar','cognito','comprehend','compute-optimizer','config','connect','console','consolemobile','containerscopilot','corretto','datapipeline','datasync','deepcomposer','deeplens','deepracer','detective','device-farm','devops-guru','directconnect','directoryservice','dms','documentdb','dynamodb','ebs','ec2','ec2autoscaling','ecr','ecs','efs','eks','ekseks-anywhere','ekseks-distro','elasticache','elasticbeanstalk','elasticloadbalancing','elasticmapreduce','elasticsearch-service','elastictranscoder','eventbridge','fargate','fis','forecast','fraud-detector','freertos','fsxlustre','fsxwindows','gamelift','global-accelerator','glue','grafana','ground-station','guardduty','iam','iot-analytics','iot-core','iot-device-defender','iot-device-management','iot-events','iot-sitewise','iot-things-graph','kendra','keyspaces','kinesis','kinesisvideo-streams','kms','lake-formation','lambda','lex','license-manager','lightsail','location','lookout-for-equipment','lookout-for-metrics','lookout-for-vision','lumberyard','machine-learningcontainers','macie','managed-blockchain','managed-workflows-for-apache-airflow','migration-evaluator','migration-hub','monitron','msk','neptune','network-firewall','opsworks','organizations','otel','outposts','panorama','personalize','pinpoint','polly','privatelink','prometheus','proton','qldb','quicksight','ram','rds','rdsaurora','rdsvmware','redshift','rekognition','robomaker','route53','s3','sagemaker','sagemakergroundtruth','security-hub','server-migration-service','servicecatalog','ses','shield','snow','sns','sqs','step-functions','storagegateway','sumerian','systems-manager','textract','timestream','transcribe','transit-gateway','translate','vpc','well-architected-tool','workdocs','worklink','workmail','workspaces','xray'];
                 callback([].concat(utils, products));
             }
