88fffb8f28a50d6aa62b12b2b7a94115fb08f0c2
dylan <dylanhogg@gmail.com>
2021-03-13 17:04:15 +1100

    Game loop implemented.

--- src/clients/javascript/app/app.js
+++ src/clients/javascript/app/app.js
@@ -2,29 +2,34 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function handleAnswer(term, answer, correct_answer) {
     if (correct_answer === null) {
         term.echo('Game has not started.');
     } else if (answer === correct_answer) {
         state_correct++;
         state_total++;
-        term.echo('Correct!');
-        term.echo('Score: ' + state_correct + "/" + state_total);
+        term.echo(box_top + box_cor + box_btm);
         // TODO: add correct streak
     } else {
         state_incorrect++;
         state_total++;
-        term.echo('Wrong :(');
-        term.echo('Correct answer was ' + correct_answer);  // TODO: convert nmeric to alpha
-        term.echo('Score: ' + state_correct + "/" + state_total);
+        term.echo(box_top + box_wro + box_btm);
+        term.echo('The correct answer was [' + correct_answer_display  + ']');
     }
 
-    term.echo("DEUBG:");
-    term.echo('answer =          ' + answer);
-    term.echo('correct_answer =  ' + correct_answer);
-    term.echo('state_incorrect = ' + answer);
-    term.echo('state_correct =   ' + answer);
-    term.echo('state_total =     ' + answer);
+    term.echo('Score: ' + state_correct + '/' + state_total);
+    term.echo('');
+
+//    term.echo("DEBUG:");
+//    term.echo('answer =          ' + answer);
+//    term.echo('correct_answer =  ' + correct_answer);
+//    term.echo('state_correct =   ' + state_correct);
+//    term.echo('state_incorrect = ' + state_incorrect);
+//    term.echo('state_total =     ' + state_total);
 }
 
 function playJeopardy(term, product, stopSpinningFn) {
@@ -53,12 +58,8 @@ function playJeopardy(term, product, stopSpinningFn) {
             data[idx3]
         ]
 
-        correct_answer = randomNumber(1,3);
-
-        var box_top = "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
-        var box_ans = "┃ Given the answer:              ┃\n";
-        var box_qns = "┃ What was the question?         ┃\n";
-        var box_btm = "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
+        correct_answer = randomNumber(0,3);
+        correct_answer_display = ["A", "B", "C"][correct_answer];
 
         // Display answer
         term.echo(box_top + box_ans + box_btm);
@@ -71,16 +72,10 @@ function playJeopardy(term, product, stopSpinningFn) {
         term.echo("[C] " + selected_qnas[2]["question"] + "\n");
 
         // Debug
-        term.echo("\nDebug:")
-        term.echo("\ncorrect_answer:" + correct_answer);
-        term.echo(selected_qnas[correct_answer]["question"]);
-        term.echo("")
-//      term.echo(selected_qnas[correct_answer]["answer"]);
-//      term.echo("")
-
-//      for (i=0; i<100; i++) {
-//          term.echo(randomNumber(0,3));
-//      }
+//        term.echo("\nDebug:")
+//        term.echo("\ncorrect_answer:" + correct_answer);
+//        term.echo(selected_qnas[correct_answer]["question"]);
+//        term.echo("")
       },
       error: function(data) {
         console.log('ajax error');
@@ -96,11 +91,23 @@ var qna_data_count = 0;
 var data_ready = false;
 var product_name = null;
 var config_spinner_name = 'dots';
-var correct_answer = null;
+// var config_prompt = 'cloud-jeopardy> ';
+// var config_prompt = '> ';
+var config_prompt = 'A, B or C? > ';
 
+var correct_answer = null;
+var correct_answer_display = null;
+var state_product = null;
 var state_correct = 0;
 var state_incorrect = 0;
 var state_total = 0;
+
+var box_top = "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+var box_ans = "┃ Given the answer:              ┃\n";
+var box_qns = "┃ What was the question?         ┃\n";
+var box_cor = "┃ Correct!                       ┃\n";
+var box_wro = "┃ Wrong, sorry.                  ┃\n";
+var box_btm = "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
 
 $(function($, undefined) {
     var spinners_url = 'spinners.json';
@@ -108,8 +115,9 @@ $(function($, undefined) {
         var animation = false;
         var timer;
         var prompt;
-        var spinner;
         var i;
+
+        var spinner = spinners[config_spinner_name];
 
         function start(term, spinner) {
             animation = true;
@@ -138,31 +146,47 @@ $(function($, undefined) {
                 this.echo('Try typing "play ec2", or "play ec" and then hitting tab twice to autocomplete.');
             },
             a: function() {
-                handleAnswer(this, 1, correct_answer)
+                handleAnswer(this, 0, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
+//                sleep(2000).then(() => {
+//                    // Do something after the sleep!
+//                    playJeopardy(this, state_product, stop);
+//                });
             },
             A: function() {
-                handleAnswer(this, 1, correct_answer)
+                handleAnswer(this, 0, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
             },
             b: function() {
-                handleAnswer(this, 2, correct_answer)
+                handleAnswer(this, 1, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
             },
             B: function() {
-                handleAnswer(this, 2, correct_answer)
+                handleAnswer(this, 1, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
             },
             c: function() {
-                handleAnswer(this, 3, correct_answer)
+                handleAnswer(this, 2, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
             },
             C: function() {
-                handleAnswer(this, 3, correct_answer)
+                handleAnswer(this, 2, correct_answer);
+                start(this, spinner);
+                playJeopardy(this, state_product, stop);
             },
             play: function(product) {
-                spinner = spinners[config_spinner_name];
+                state_product = product;
                 start(this, spinner);
                 playJeopardy(this, product, stop);
             }
         }, {
             name: 'cloud_jeopardy',
-            prompt: 'cloud-jeopardy> ',
+            prompt: config_prompt,
             greetings: 'Welcome to Cloud Jeopardy!\n\n' +
                 'An AWS Certification study tool - select the correct question for the given AWS FAQ answer, Jeopardy style.\n',
             scrollOnEcho: true,
@@ -170,7 +194,14 @@ $(function($, undefined) {
                 var utils = ['help', 'status', 'play'];
                 var products = ['amazon-mq','amplify','api-gateway','app-mesh','app2container','appflow','application-discovery','appstream2','appsync','athena','audit-manager','augmented-ai','autoscaling','aws-transfer-family','backup','batch','braket','cdk','certificate-manager','chatbot','chime','cloud9','cloudformation','cloudfront','cloudhsm','cloudsearch','cloudshell','cloudtrail','cloudwatch','codebuild','codecommit','codedeploy','codeguru','codepipeline','codestar','cognito','comprehend','compute-optimizer','config','connect','console','consolemobile','containerscopilot','corretto','datapipeline','datasync','deepcomposer','deeplens','deepracer','detective','device-farm','devops-guru','directconnect','directoryservice','dms','documentdb','dynamodb','ebs','ec2','ec2autoscaling','ecr','ecs','efs','eks','ekseks-anywhere','ekseks-distro','elasticache','elasticbeanstalk','elasticloadbalancing','elasticmapreduce','elasticsearch-service','elastictranscoder','eventbridge','fargate','fis','forecast','fraud-detector','freertos','fsxlustre','fsxwindows','gamelift','global-accelerator','glue','grafana','ground-station','guardduty','iam','iot-analytics','iot-core','iot-device-defender','iot-device-management','iot-events','iot-sitewise','iot-things-graph','kendra','keyspaces','kinesis','kinesisvideo-streams','kms','lake-formation','lambda','lex','license-manager','lightsail','location','lookout-for-equipment','lookout-for-metrics','lookout-for-vision','lumberyard','machine-learningcontainers','macie','managed-blockchain','managed-workflows-for-apache-airflow','migration-evaluator','migration-hub','monitron','msk','neptune','network-firewall','opsworks','organizations','otel','outposts','panorama','personalize','pinpoint','polly','privatelink','prometheus','proton','qldb','quicksight','ram','rds','rdsaurora','rdsvmware','redshift','rekognition','robomaker','route53','s3','sagemaker','sagemakergroundtruth','security-hub','server-migration-service','servicecatalog','ses','shield','snow','sns','sqs','step-functions','storagegateway','sumerian','systems-manager','textract','timestream','transcribe','transit-gateway','translate','vpc','well-architected-tool','workdocs','worklink','workmail','workspaces','xray'];
                 callback([].concat(utils, products));
+            },
+            keydown: function(e) {
+                //disable keyboard when animating
+                if (animation) {
+                    return false;
+                }
             }
         });
+
     });
 });
